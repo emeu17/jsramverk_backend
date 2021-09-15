800115e00796d5ff4c34ee75a87f0474f7d28a12
emeu17 <emeu17@student.bth.se>
2021-09-15 13:38:41 +0100

    updated tests, added travis.yml

---
+++ test/docs.js
@@ -0,0 +1,52 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Reports', () => {
+    describe('GET /docs', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/docs")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("array");
+                    res.body.length.should.be.above(0);
+                    done();
+                });
+        });
+    });
+
+    describe('POST /docs', () => {
+        it('201 HAPPY PATH', (done) => {
+            chai.request(server)
+                .post("/docs")
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    // console.log(res.body.data);
+                    // console.log(typeof res.body.data.msg);
+                    res.body.data.msg.should.be.an("string");
+                    done();
+                });
+        });
+    });
+
+    describe('PUT /docs', () => {
+        it('204 HAPPY PATH', (done) => {
+            chai.request(server)
+                .put("/docs")
+                .end((err, res) => {
+                    res.should.have.status(204);
+                    res.body.should.be.an("object");
+
+                    done();
+                });
+        });
+    });
+});
---
+++ test/list.js
@@ -0,0 +1,27 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Reports', () => {
+    describe('GET /list', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/list")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    // console.log(typeof res.body);
+                    res.body.should.be.an("array");
+                    // res.body.data.should.be.an("array");
+                    res.body.length.should.be.above(0);
+
+                    done();
+                });
+        });
+    });
+});
